126ba18253504b05a4b1a856646127453e6db54e
Amirhan Botagariev <ruby.mainacc@gmail.com>
2025-02-04 23:00:02 +0500

    Created users table

--- platform/main.py
+++ platform/main.py
@@ -5,12 +5,18 @@ from fastapi import FastAPI
 
 from api import router as api_router
 from core.config import settings
-from core.models import db_helper
+from core.models import (
+    db_helper,
+    Base,
+    User,
+)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # startup
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)  # Пересоздаём заново
     yield
     # shutdown
     await db_helper.dispose()
