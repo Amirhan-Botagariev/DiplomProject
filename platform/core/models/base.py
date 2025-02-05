b9a0d0c03e1d3de510958d4d1e03c4e2a0a0ca6d
Amirhan Botagariev <ruby.mainacc@gmail.com>
2025-02-05 22:29:54 +0500

    Added naming conventions for keys in DB

--- platform/core/models/base.py
+++ platform/core/models/base.py
@@ -1,8 +1,15 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import Mapped, DeclarativeBase, mapped_column, declared_attr
+
+from core.config import settings
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention,
+    )
 
     id: Mapped[int] = mapped_column(primary_key=True)
 
