d6e41f21dc8abd4d4563d9843bed125bef2a03cd
Amirhan Botagariev <ruby.mainacc@gmail.com>
2025-02-13 22:35:12 +0500

    Added method for user database

--- platform/core/models/user.py
+++ platform/core/models/user.py
@@ -1,6 +1,15 @@
+from typing import TYPE_CHECKING
+
 from .base import Base
-from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class User(Base, SQLAlchemyBaseUserTable[int]):
     pass
+
+    @classmethod
+    def get_db(cls, session: AsyncSession):
+        return SQLAlchemyUserDatabase(session, User)
