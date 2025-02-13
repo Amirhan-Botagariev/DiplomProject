8776363ad1d3845cd4b09ed5c56781bdb479b6e9
Amirhan Botagariev <ruby.mainacc@gmail.com>
2025-02-13 22:53:53 +0500

    Created UserIdType var

--- platform/core/models/user.py
+++ platform/core/models/user.py
@@ -2,12 +2,13 @@ from typing import TYPE_CHECKING
 from sqlalchemy.ext.asyncio import AsyncSession
 from .base import Base
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from core.types.user_id import UserIdType
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
-class User(Base, SQLAlchemyBaseUserTable[int]):
+class User(Base, SQLAlchemyBaseUserTable[UserIdType]):
     pass
 
     @classmethod
