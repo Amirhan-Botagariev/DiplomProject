b7fcfb18515a0a952dd53032c5b75a382b59eba1
Amirhan Botagariev <ruby.mainacc@gmail.com>
2025-02-14 21:05:51 +0500

    Change users router

--- platform/api/api_v1/users.py
+++ platform/api/api_v1/users.py
@@ -1,37 +1,18 @@
-from typing import Annotated
-
 from fastapi import APIRouter
-from fastapi.params import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
 
-import crud.users as users_crud
-from core.models import db_helper
-from core.schemas.user import UserRead, UserCreate
+from api.api_v1.fastapi_users_routers import fastapi_users
+from core.config import settings
+from core.schemas.user import UserRead, UserUpdate
 
-router = APIRouter()
+router = APIRouter(
+    prefix=settings.api.v1.users,
+)
 
-
-@router.get("/", response_model=list[UserRead])
-async def get_users(
-    session: Annotated[
-        AsyncSession,
-        Depends(db_helper.session_getter),
-    ],
-):
-    users = await users_crud.get_all_users(session=session)
-    return users
-
-
-@router.post("/", response_model=UserRead)
-async def create_user(
-    session: Annotated[
-        AsyncSession,
-        Depends(db_helper.session_getter),
-    ],
-    user_create: UserCreate,
-):
-    user = await users_crud.create_user(
-        session=session,
-        user_create=user_create,
-    )
-    return user
+# /me
+# /{id}
+router.include_router(
+    router=fastapi_users.get_users_router(
+        UserRead,
+        UserUpdate,
+    ),
+)
