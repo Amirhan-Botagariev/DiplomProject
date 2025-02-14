0fe4526067a771c2e00725f628dc45f5d739fb81
Amirhan Botagariev <ruby.mainacc@gmail.com>
2025-02-14 14:38:18 +0500

    Included auth router from fast api routers

--- platform/api/api_v1/auth.py
+++ platform/api/api_v1/auth.py
@@ -1,5 +1,10 @@
 from fastapi import APIRouter
 
-from core.config import settings
+from api.api_v1.fastapi_users_routers import fastapi_users
+from api.dependencies.authentication.backend import authentication_backend
 
 router = APIRouter()
+
+router.include_router(
+    fastapi_users.get_auth_router(authentication_backend),
+)
