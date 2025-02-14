9a00d16f569c06d88614e1af5801d1a62f441419
Amirhan Botagariev <ruby.mainacc@gmail.com>
2025-02-14 23:42:18 +0500

    Create messages router

--- platform/api/api_v1/__init__.py
+++ platform/api/api_v1/__init__.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from core.config import settings
 from .users import router as users_router
 from .auth import router as auth_router
+from .messages import router as messages_router
 
 router = APIRouter(
     prefix=settings.api.v1.prefix,
@@ -17,4 +18,10 @@ router.include_router(
     auth_router,
     prefix=settings.api.v1.auth,
     tags=["Auth"],
+)
+
+router.include_router(
+    messages_router,
+    prefix=settings.api.v1.messages,
+    tags=["Messages"],
 )
---
+++ platform/api/api_v1/messages.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+def get_user_messages():
+    pass
