f45d433829536e248f0e4cd4bbcb8ce83adc9e8c
Amirhan Botagariev <ruby.mainacc@gmail.com>
2025-02-14 21:13:15 +0500

    Added http bearer dependency for token

--- platform/api/__init__.py
+++ platform/api/__init__.py
@@ -1,6 +1,11 @@
 from fastapi import APIRouter
+from fastapi.params import Depends
+from fastapi.security import HTTPBearer
 
 from .api_v1 import router as router_api_v1
 
-router = APIRouter()
+http_bearer = HTTPBearer(auto_error=False)
+router = APIRouter(
+    dependencies=[Depends(http_bearer)],
+)
 router.include_router(router_api_v1)
