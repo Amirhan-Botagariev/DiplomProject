83cb02777b62d09479191f3ee7e4e3d4f9abd07c
ru5t3m <zharmagambetovrustem@gmail.com>
2025-04-20 17:06:10 +0500

    model edits

--- platform_backend/core/models/employees/department.py
+++ platform_backend/core/models/employees/department.py
@@ -1,4 +1,3 @@
-# models/department.py
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from core.models import Base
@@ -10,5 +9,4 @@ class Department(Base):
     department_id = Column(Integer, primary_key=True, index=True)
     department_name = Column(String(50), nullable=False)
 
-    # Optional: если хочешь связать с employee
     employees = relationship("Employee", back_populates="department")
