385c521eb9686a3b3a93232b455b23f5b34dd169
ru5t3m <zharmagambetovrustem@gmail.com>
2025-04-20 16:56:08 +0500

    EducationField model

--- platform_backend/core/models/employees/education_field.py
+++ platform_backend/core/models/employees/education_field.py
@@ -7,6 +7,6 @@ class EducationField(Base):
     __tablename__ = "education_fields"
 
     education_field_id = Column(Integer, primary_key=True, index=True)
-    education_field_name = Column(String, nullable=False)
+    education_field_name = Column(String(50), nullable=False, unique=True)
 
-    employees = relationship("Employee", back_populates="education_field")
+    employees = relationship("Employee", back_populates="education_field", lazy="joined")
