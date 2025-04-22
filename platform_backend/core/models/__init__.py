c114f87e25cf5b805f14d4cd52296ed342984920
Amirhan Botagariev <ruby.mainacc@gmail.com>
2025-04-22 22:52:42 +0500

    Добавил миграцию для создания новых таблиц из дампа данных

--- platform_backend/core/models/__init__.py
+++ platform_backend/core/models/__init__.py
@@ -3,9 +3,29 @@ _all_ = (
     "Base",
     "db_helper",
     "User",
+    "Employee",
+    "AttritionPrediction",
+    "JobRole",
+    "Gender",
+    "BusinessTravel",
+    "MaritalStatus",
+    "EducationField",
+    "Salary",
+    "PerformanceReview",
 )
 
 from .db_helper import db_helper
 from .base import Base
 from .user import User
 from .access_token import AccessToken
+
+from .employees.employee import Employee
+from .employees.attrition_prediction import AttritionPrediction
+from .employees.department import Department
+from .employees.job_role import JobRole
+from .employees.gender import Gender
+from .employees.business_travel import BusinessTravel
+from .employees.marital_status import MaritalStatus
+from .employees.education_field import EducationField
+from .employees.salary import Salary
+from .employees.performance_review import PerformanceReview
