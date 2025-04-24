3b516de457db660476c43c5ccc04f73b7176b41a
ru5t3m <zharmagambetovrustem@gmail.com>
2025-04-24 20:38:44 +0500

    disable console logging in logger.py file itself

--- platform_backend/core/gunicorn/logger.py
+++ platform_backend/core/gunicorn/logger.py
@@ -23,31 +23,18 @@ class GunicornLogger(Logger):
             access_handler = RotatingFileHandler(
                 settings.logging.access_log_file,
                 maxBytes=settings.logging.max_file_size_mb * 1024 * 1024,
-                backupCount=settings.logging.backup_count
+                backupCount=settings.logging.backup_count,
             )
             access_handler.setFormatter(formatter)
             self.access_log.addHandler(access_handler)
 
-            # Configure error log
             error_handler = RotatingFileHandler(
                 settings.logging.error_log_file,
                 maxBytes=settings.logging.max_file_size_mb * 1024 * 1024,
-                backupCount=settings.logging.backup_count
+                backupCount=settings.logging.backup_count,
             )
             error_handler.setFormatter(formatter)
             self.error_log.addHandler(error_handler)
-
-        if settings.logging.console_logging:
-            self._set_handler(
-                log=self.access_log,
-                output=cfg.accesslog or "-",
-                fmt=formatter,
-            )
-            self._set_handler(
-                log=self.error_log,
-                output=cfg.errorlog or "-",
-                fmt=formatter,
-            )
 
         self.error_log.setLevel(settings.logging.log_level.upper())
         self.access_log.setLevel(settings.logging.log_level.upper())
