f3f5d5934f04059caeb2979cc8741d603c22d795
ru5t3m <zharmagambetovrustem@gmail.com>
2025-04-24 20:52:16 +0500

    disable logs from terminal

--- platform_backend/core/gunicorn/app_options.py
+++ platform_backend/core/gunicorn/app_options.py
@@ -5,13 +5,14 @@ def get_app_options(
     host: str, port: int, workers: int, timeout: int, log_level: str, reload: bool
 ) -> dict:
     return {
-        "accesslog": "-",
-        "errorlog": "-",
+        "accesslog": None,  # Disable access log output to stdout
+        "errorlog": None,  # Disable error log output to stderr
         "bind": f"{host}:{port}",
         "timeout": timeout,
         "workers": workers,
         "loglevel": log_level,
         "logger_class": GunicornLogger,
         "worker_class": "uvicorn.workers.UvicornWorker",
-        "reload": reload
+        "reload": reload,
+        "capture_output": True,  # Capture and redirect stdout/stderr to log files
     }
